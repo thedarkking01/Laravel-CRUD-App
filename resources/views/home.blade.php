87f4cf576a0eec6651417fe0ebd35c18cd6e0974
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-02-24 16:26:44 +0530

    create blog post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,16 @@
         @csrf
         <button type="submit">Logout</button>
     </form>
+
+    <div style="border: 3px solid black;">
+    <h2>Create a New Post</h2>
+        <form action="/create-post" method="POST">
+        @csrf
+            <input type="text" name="title" placeholder="post title">
+            <textarea name="body" placeholder="body content..."></textarea>
+            <button>Save Post</button>
+        </form>
+    </div>
     @else
     <div style="border: 3px solid black;">
         <h2>Register</h2>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -15,3 +16,6 @@ Route::get('/', function () {
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/logout', [UserController::class, 'logout']);
 Route::post('/login', [UserController::class, 'login']);
+
+// Blog post related routes
+Route::post('/create-post',[PostController::class,'createPost']);
