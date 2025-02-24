828120ada162ee239b0be6c26de17c2fb973cf46
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-02-24 20:35:18 +0530

    Add feature of delete post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function deletePost(Post $post) {
+        if (auth()->user()->id === $post['user_id']) {
+            $post->delete();
+        }
+        return redirect('/');
+    }
+
     public function actuallyUpdatePost(Post $post, Request $request) {
         if (auth()->user()->id !== $post['user_id']) {
             return redirect('/');
